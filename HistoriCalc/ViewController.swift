882cf543c62ed3051894d9502b2b01534a5c494f
kaori baba <kaori@kaori-no-MacBook.local>
2016-12-17 19:09:44 +0900

    Initial Commit

---
+++ HistoriCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HistoriCalc
+//
+//  Created by kaori baba on 2016/12/17.
+//  Copyright © 2016年 kaori baba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
